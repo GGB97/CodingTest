f130061c3f7aa92733b7f73e3116a2e7eaabf90e
GGB <99232361+GGB97@users.noreply.github.com>
2024-01-17 10:04:11 +0900

    [level 1] Title: 시저 암호, Time: 0.53 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cs
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cs
@@ -1,31 +1,22 @@
-using System.Text;
-
 public class Solution {
     public string solution(string s, int n) {
-        int asc;
-        StringBuilder sb = new StringBuilder();
 
-        foreach (char c in s)
-        {
-            asc = (int)c;
-            if(65 <= asc && asc <= 90)
-            {
-                asc += n;
-                if (90 < asc)
-                    asc -= 26;
-            }
-            else if (97 <= asc && asc <=122)
-            {
-                asc += n;
-                if (asc > 122)
-                    asc -= 26;
-            }
-            else if (asc - n == 32)
-                asc = 32;
-
-            sb.Append((char)asc);
-        }
-
-        return sb.ToString();
+                string answer = "";
+                char[] temp = s.ToCharArray();
+                for(int i = 0; i < s.Length; ++i)
+                {
+                    if ( (temp[i] >= 'a' && temp[i] <= 'z')|| (temp[i] >= 'A' && temp[i] <= 'Z'))
+                    {
+                        if (char.IsUpper(temp[i]))
+                        {
+                            temp[i] = (char)((temp[i] + n - 'A') % 26 + 'A');
+                        }
+                        else
+                        {
+                            temp[i] = (char)((temp[i] + n - 'a') % 26 + 'a');
+                        }
+                    }
+                }
+                return new string(temp);
     }
 }
